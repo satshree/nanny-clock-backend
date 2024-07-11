ac0df4babcb82bc1518386d63873c64a5dec5d2a
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-07-10 19:50:17 -0500

    setup firebase

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
+const { verifyToken } = require("./firebase/middlewares");
 
+// EXPRESS APP
 const app = express();
 app.use(bodyParser.json()); // Parse JSON requests
+
+// CORS
+app.use(cors());
 
 // VIEW ENGINE
 app.set("view engine", "ejs");
 
+// FIREBASE AUTHENTICATION
+app.use(verifyToken);
+
 // SERVER
-const port = process.env.PORT || 3000; // Use environment variable or default port
+const port = process.env.PORT || 8000; // Use environment variable or default port
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
---
+++ firebase/middlewares.js
@@ -0,0 +1,22 @@
+const admin = require("./admin");
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    console.log("ERR", error);
+    return res.status(403).send("Invalid or expired token");
+  }
+};
+
+module.exports = { verifyToken };
